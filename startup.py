42d98524ec591165524ab7cbf4f8e3e14961c2ed
Michael Fulton <mfulton07@qub.ac.uk>
2021-12-14 11:13:44 +0000

    Adding example.

--- startup.py
+++ startup.py
@@ -10,29 +10,35 @@ from astrorapid.classify import Classify
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run AstroRAPID on an object from the ZTF forced Photometry')
 
-    parser.add_argument('-n', dest='filename', help='Name of the ZTF forced photometry .txt file to fit with RAPID. File must be inside candidates direcotry. Mandatory.', default = 'ZTF19abheamc')
-    parser.add_argument('-z', dest='redshift', help=' Spectroscopic or photometric redshift of object or host. Optional.')
+    parser.add_argument('-n', dest='filename_str', help='Name of the ZTF forced photometry .txt file to fit with RAPID. File must be inside candidates direcotry. Mandatory.', default = 'ZTF21aaqytjr')
+    parser.add_argument('-z', dest='redshift_str', help='Spectroscopic or photometric redshift of the object or host. Optional.', default = '0.003319')
+    parser.add_argument('-ra', dest='ra_deg_str', help='Right Ascension of the object in degrees. Mandatory', default = '186.423654	')
+    parser.add_argument('-dec', dest='dec_deg_str', help='Declination of the object in degrees. Mandatory', default = '7.228395')
+    parser.add_argument('-mwebv', dest='ebv_str', help='Total galactic visual extinction (mwebv) in the line-of-sight of the object. Mandatory', default = '0.2851')
     parser.add_argument('-v', dest='verbose', action='store_true', help='Turn on verbose mode (default: Off).')
 
     args = parser.parse_args()
 
     # check if file exists
-    obj = args.filename
-    filepath = os.path.dirname(os.path.abspath(__file__)) + f'/candidates/{obj}.txt'
+    objid = args.filename_str
+    filepath = os.path.dirname(os.path.abspath(__file__)) + f'/candidates/{objid}.txt'
     if os.path.isfile(filepath) : 
-      print(f'{obj}.txt file found!')
+      print(f'{objid}.txt file found!')
     else:
-      print(f'{obj}.txt file not found!')
+      print(f'{objid}.txt file not found!')
       quit()
     
 if args.verbose:
   print(args)
 
-# Convert the redshift into a number if one is provided
+# Convert the ra, dec, dust reddening and redshift into a number if one is provided
+ra = float(args.ra_deg_str)
+dec = float(args.dec_deg_str)
+mwebv = float(args.ebv_str)
 try:
-  z = float(args.redshift)
+  redshift = float(args.redshift_str)
 except:
-  z = 'none'
+  redshift = 0.0
 
 # Read in forced photometry file and drop comment lines
 data = []
@@ -57,28 +63,42 @@ semi_df = [[floatify(x) for x in e.split()] for e in data]
 full_df = pd.DataFrame.from_records(semi_df)
 full_df.columns = headers
 
-print(full_df)
+# change working directory to the outputs folder
+outpath = os.getcwd() + '/outputs/'
+os.chdir(outpath)
 
-# Light curve information. It may be easier if this is read from a file and later manipulated into this format.
-mjd = [57433.4816, 57436.4815, 57439.4817, 57451.4604, 57454.4397, 57459.3963, 57462.418 , 57465.4385, 57468.3768, 57473.3606, 57487.3364, 57490.3341, 57493.3154, 57496.3352, 57505.3144, 57513.2542, 57532.2717, 57536.2531, 57543.2545, 57546.2703, 57551.2115, 57555.2669, 57558.2769, 57561.1899, 57573.2133,57433.5019, 57436.4609, 57439.4587, 57444.4357, 57459.4189, 57468.3142, 57476.355 , 57479.3568, 57487.3586, 57490.3562, 57493.3352, 57496.2949, 57505.3557, 57509.2932, 57513.2934, 57518.2735, 57521.2739, 57536.2321, 57539.2115, 57543.2301, 57551.1701, 57555.2107, 57558.191 , 57573.1923, 57576.1749, 57586.1854]
-flux = [2.0357230e+00, -2.0382695e+00,  1.0084588e+02,  5.5482742e+01,  1.4867026e+01, -6.5136810e+01,  1.6740545e+01, -5.7269131e+01,  1.0649184e+02,  1.5505235e+02,  3.2445984e+02,  2.8735449e+02,  2.0898877e+02,  2.8958893e+02,  1.9793906e+02, -1.3370536e+01, -3.9001358e+01,  7.4040916e+01, -1.7343750e+00,  2.7844931e+01,  6.0861992e+01,  4.2057487e+01,  7.1565346e+01, -2.6085690e-01, -6.8435440e+01, 17.573107  ,   41.445435  , -110.72664   ,  111.328964  ,  -63.48336   ,  352.44907   ,  199.59058   ,  429.83075   ,  338.5255    ,  409.94604   ,  389.71262   ,  195.63905   ,  267.13318   ,  123.92461   ,  200.3431    ,  106.994514  ,  142.96387   ,   56.491238  ,   55.17521   ,   97.556946  ,  -29.263103  ,  142.57687   ,  -20.85057   ,   -0.67210346,   63.353024  ,  -40.02601]
-fluxerr = [42.784702,  43.83665 ,  99.98704 ,  45.26248 ,  43.040398,  44.00679 ,  41.856007,  49.354336, 105.86439 , 114.0044  ,  45.697918,  44.15781 ,  60.574158,  93.08788 ,  66.04482 ,  44.26264 ,  91.525085,  42.768955,  43.228336,  44.178196,  62.15593 , 109.270035, 174.49638 ,  72.6023  ,  48.021034, 44.86118 ,  48.659588, 100.97703 , 148.94061 ,  44.98218 , 139.11194 ,  71.4585  ,  47.766987,  45.77923 ,  45.610615,  60.50458 , 105.11658 ,  71.41217 ,  43.945534,  45.154167,  43.84058 ,  52.93122 ,  44.722775,  44.250145,  43.95989 ,  68.101326, 127.122025, 124.1893  ,  49.952255,  54.50728 , 114.91599]
-passband = ['g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r']
-photflag = [0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 4096, 4096,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 0,    0,    0,    0,    0,    0,    0,    0,    0, 4096, 6144, 4096, 4096, 4096, 0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0, 0,    0,    0,    0]
-objid = 'transient_1'
-ra = 3.75464531293933
-dec = 0.205076187109334
-redshift = 0.233557
-mwebv = 0.0228761
+# extract from the dataframe and create the mjd, flux, fluxerr and passband lists that rapid needs for making the lightcurve
+mjd = list(full_df["jd"] - 2400000.5)
+flux = list(full_df["forcediffimflux"])
+fluxerr = list(full_df["forcediffimfluxunc"])
+filters = list(full_df["filter"])
+passband = [f.replace("ZTF_", "") for f in filters]
 
-light_curve_info1 = (mjd, flux, fluxerr, passband, photflag, ra, dec, objid, redshift, mwebv)
+# use the flux and fluxerr list to create the list of photo flags (0 - non detection, 4096 - normal detection, 6144 - trigger detection)
+photflag = []
+trigger = 0
+for idx, val in enumerate(flux):
+  # if flux is not a 3-sig detection then flag as a non detection
+  # else flag as a normal detection
+  if val < 3*fluxerr[idx]:
+    photflag.append(0)
+  else:
+    # but if flux is the first 5-sig detection then flag as the trigger detection
+    if val >= 5*fluxerr[idx] and trigger == 0:
+      trigger = 1
+      photflag.append(6144)
+    else:
+      photflag.append(4096)
+
+# Light curve information for RAPID
+light_curve_info = (mjd, flux, fluxerr, passband, photflag, ra, dec, objid, redshift, mwebv)
 
 # Classification
-light_curve_list = [light_curve_info1,]  # Add more light curves to be classified to this list.
+light_curve_list = [light_curve_info, ]  # Add more light curves to be classified to this list
 
 classification = Classify(known_redshift=True)
 predictions = classification.get_predictions(light_curve_list)
-print(predictions)
+#print(predictions)
 
 # Plot classifications vs time
 classification.plot_light_curves_and_classifications()
